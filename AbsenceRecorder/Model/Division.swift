558cec74ee68a9d1cfdd92ec2d0ef614bde9b801
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-01-24 20:46:11 +0000

    Adding basic model for divisions and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Nicholas Hodson on 24/01/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
