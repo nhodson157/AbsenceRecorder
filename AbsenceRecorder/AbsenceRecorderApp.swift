9e33f080f6ab3d6d5bc84183d9bbedba2349eae7
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-01-24 18:44:25 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Nicholas Hodson on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
