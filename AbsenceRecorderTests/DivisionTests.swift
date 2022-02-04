3b2f24c6e86e20fb112c21973271a5e29cc768b2
Nick Hodson <hodson.n@etoncollege.org.uk>
2022-02-04 12:30:06 +0000

    Absence recorder - Part 4 EW working

---
+++ AbsenceRecorderTests/DivisionTests.swift
@@ -0,0 +1,33 @@
+//
+//  DivisionTests.swift
+//  AbsenceRecorderTests
+//
+//  Created by Nicholas Hodson on 04/02/2022.
+//
+
+import XCTest
+@testable import AbsenceRecorder
+
+class DivisionTests: XCTestCase {
+
+    func testGetAbsenceDivisionWithAbsenceOnThatDayReturnsAbsence() throws {
+        let division = Division(code: "TestDiv")
+        let dateToday = Date()
+        let dateTomorrow = Date(timeIntervalSinceNow: 1000000000)
+        let dateLaterToday = Date(timeIntervalSinceNow: 100)
+        let absence1 = Absence(date: dateTomorrow, students: Student.examples)
+        let absence2 = Absence(date: dateToday, students: Student.examples)
+        division.absences.append(absence1)
+        division.absences.append(absence2)
+        
+        let actual = division.getAbsence(for: dateLaterToday)
+        
+        XCTAssertNotNil(actual)
+        if let actualNotNil = actual {
+            XCTAssertEqual(actualNotNil.takenOn, absence2.takenOn)
+        }
+    }
+
+    
+
+}
